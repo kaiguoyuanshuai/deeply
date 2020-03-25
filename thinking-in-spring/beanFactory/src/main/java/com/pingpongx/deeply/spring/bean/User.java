399aacb9e88cb7d8f5bd15711a7c2aaea155000e
zhengkk <zhengkk@pingpongx.com>
2020-03-25 13:33:32 +0800

    ConfigurableBeanFactory 描述

---
+++ thinking-in-spring/beanFactory/src/main/java/com/pingpongx/deeply/spring/bean/User.java
@@ -0,0 +1,29 @@
+package com.pingpongx.deeply.spring.bean;
+
+/**
+ * @author <a href="mailto:zhengkk@pingpongx.com">zhengkk</a>
+ * @since 2020-03-24 20:34
+ **/
+
+public class User {
+
+    private String age;
+
+    private String name;
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
--- thinking-in-spring/beanFactory/src/main/java/com/pingpongx/deeply/spring/beanFactory/ConfigurableBeanFactoryDemo.java
+++ thinking-in-spring/beanFactory/src/main/java/com/pingpongx/deeply/spring/beanFactory/ConfigurableBeanFactoryDemo.java
@@ -5,9 +5,12 @@ import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  *
+ * ParentBeanFactory 不能获取 当前BeanFactory定义的Bean
+ * 当前BeanFactory 可以获取 ParentBeanFactory 定义的Bean
  * @author <a href="mailto:zhengkk@pingpongx.com">zhengkk</a>
  * @since 2020-03-20 13:12
  **/
@@ -19,28 +22,31 @@ public class ConfigurableBeanFactoryDemo {
             .buildContextAndRefresh(String.class);
         ConfigurableBeanFactory beanFactory = annotationConfigApplicationContext.getBeanFactory();
 
-        AnnotationConfigApplicationContext anotherContext = buildOneMoreBeanFactory();
+        //构建另外一个 ApplicationContext
+        ClassPathXmlApplicationContext anotherContext = buildOneMoreBeanFactory();
+        //设置到 ParentBeanFactory
         beanFactory.setParentBeanFactory(anotherContext);
 
+        // 获取 ParentBeanFactory
         BeanFactory parentBeanFactory = beanFactory.getParentBeanFactory();
-        System.out.println(parentBeanFactory.getBean("user"));
+        System.out.println("==============parentBeanFactory 获取定义在 ParentBeanFactory 中的Bean : " + parentBeanFactory.getBean("user"));
+        System.out.println("==============当前beanFactory 获取定义在 ParentBeanFactory 中的Bean : " + beanFactory.getBean("user"));
 
-        System.out.println(beanFactory.getBean("user"));
+        System.out.println("==============parentBeanFactory 获取定义在 当前 BeanFactory 中的Bean : ");
+        System.out.println(beanFactory.getBean("tt"));
 
         annotationConfigApplicationContext.close();
     }
 
 
-    static AnnotationConfigApplicationContext buildOneMoreBeanFactory() {
-        AnnotationConfigApplicationContext annotationConfigApplicationContext = CommonAnnotationConfigApplicationContextBuilder
-            .buildContextAndRefresh(ConfigurableBeanFactoryDemo.class);
-
+    static ClassPathXmlApplicationContext buildOneMoreBeanFactory() {
+        ClassPathXmlApplicationContext annotationConfigApplicationContext = new ClassPathXmlApplicationContext("META-INF/application-parent-beanFactory.xml");
         return annotationConfigApplicationContext;
     }
 
 
     @Bean
-    public String user() {
+    public String tt() {
         return "uu";
     }
 
